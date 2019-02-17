fd832da6f633a91454597278bf800c9f99949004
Manuel Sidler <sidler.manuel@gmail.com>
2019-02-17 20:34:44 +0100

    add UWP project

---
+++ src/IPWPrototype.Windows/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace IPWPrototype.Windows
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ZXing.Net.Mobile.Forms.WindowsUniversal.ZXingScannerViewRenderer.Init();
+
+            LoadApplication(new IPWPrototype.App());
+        }
+    }
+}
